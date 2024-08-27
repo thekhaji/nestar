bf7577542338b98d03e789ce7ac7f6ecc5b94214
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-08-27 18:15:39 +0900

    feat: standard integration of logging interceptor

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from 'apps/nestar-api/src/libs/interceptor/Logging.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NestarBatchModule } from './nestar-batch.module';
-import { ValidationPipe } from '@nestjs/common';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
-  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
