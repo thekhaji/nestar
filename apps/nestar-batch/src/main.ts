449cceeb0179e8c150ce92e7b0420f59f56f650d
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-08-27 16:25:32 +0900

    fix: integrate global pipe validation

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestarBatchModule } from './nestar-batch.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
