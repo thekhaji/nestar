b0c09ed828dddc09d57b8ece1aabc20927967801
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-08-30 00:55:24 +0900

    feat: integration o Authentication and Authorization

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,8 +1,11 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException} from '@nestjs/common';
+import { InternalServerErrorException, UseGuards} from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 
 
@@ -24,11 +27,24 @@ export class MemberResolver {
     }
 
 
-    //Authenticated
+    @UseGuards(AuthGuard)
     @Mutation(()=>String)
-    public async updateMember(): Promise<string>{
+    public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string>{
         console.log("Mutation: updateMember");
+        console.log(typeof memberId);
+        
+        console.log("memberId:", memberId);
+        
         return this.memberService.updateMember();
+    }
+
+    @UseGuards(AuthGuard)
+    @Query(()=>String)
+    public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string>{
+        console.log("Mutation: checkAuth");
+        console.log("memberNick:", memberNick);
+        
+        return `Hi ${memberNick}`;
     }
 
     @Query(()=>String)
