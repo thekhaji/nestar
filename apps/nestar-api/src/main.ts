ab8ac9342c21f374f86373f0f1816d338b2789df
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-09-19 20:25:29 +0900

    feat: create WebSocket Connection process

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -4,6 +4,7 @@ import { LoggingInterceptor } from 'apps/nestar-api/src/libs/interceptor/Logging
 import { ValidationPipe } from '@nestjs/common';
 import {graphqlUploadExpress} from 'graphql-upload';
 import * as express from 'express';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,7 @@ async function bootstrap() {
 
   app.use(graphqlUploadExpress({maxFileSize: 15000000, maxFiles: 10}));
   app.use('/uploads', express.static('./uploads'));
-
+  app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
---
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -0,0 +1,27 @@
+import { Logger } from '@nestjs/common';
+import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Server } from 'ws';
+
+@WebSocketGateway({transports: ['websocket'], secure: false})
+export class SocketGateway {
+  private logger: Logger = new Logger('SocketEventsGateway');
+  private summaryClient: number = 0;
+
+  public afterInit(server: Server){
+    this.logger.log(`WebSocket Server Initialized total: ${this.summaryClient}`);
+  }
+
+  handleConnection(client: WebSocket, ...args:any[]){
+    this.summaryClient++;
+    this.logger.log(`== Client connected total: ${this.summaryClient}`);
+  }
+
+  handleDisconnect(client: WebSocket){
+    this.summaryClient--;
+    this.logger.log(`== Client disconnected total: ${this.summaryClient}`);
+  }
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): string {
+    return 'Hello world!';
+  }
+}
